e42fb2a875dd3b802bf05b8767755b0b4ab202bd
roman <adhikariroman01m@gmail.com>
2026-03-19 18:09:27 +0545

    feat: add create post functionality with toggle visibility in HomePage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -5,12 +5,13 @@ import CreatePost from "./CreatePost";
 
 const HomePage = () => {
 	const [feedType, setFeedType] = useState("forYou");
+	const [showCreatePost, setShowCreatePost] = useState(false);
 
 	return (
 		<>
-			<div className='flex-[4_4_0] mr-auto border-r border-base-300 min-h-screen'>
+			<div className='flex-[4_4_0] mr-auto border-r-2 border-base-content/20 min-h-screen'>
 				{/* Header */}
-				<div className='flex w-full border-b border-base-300 items-center'>
+				<div className='flex w-full border-b-2 border-base-content/20 items-center'>
 					<div className='flex flex-1'>
 						<div
 							className='flex justify-center flex-1 p-3 hover:bg-base-200 transition duration-300 cursor-pointer relative'
@@ -33,8 +34,31 @@ const HomePage = () => {
 					</div>
 				</div>
 
-				{/*  CREATE POST INPUT */}
-				<CreatePost />
+				{/* CREATE POST TRIGGER */}
+				<div className='p-4 border-b-2 border-base-content/20'>
+					{!showCreatePost ? (
+						<button
+							type='button'
+							className='btn btn-primary btn-sm rounded-full'
+							onClick={() => setShowCreatePost(true)}
+						>
+							Create a new post.
+						</button>
+					) : (
+						<div className='space-y-3'>
+							<div className='flex justify-end'>
+								<button
+									type='button'
+									className='btn btn-ghost btn-xs'
+									onClick={() => setShowCreatePost(false)}
+								>
+									Hide form
+								</button>
+							</div>
+							<CreatePost />
+						</div>
+					)}
+				</div>
 
 				{/* POSTS */}
 				<Posts feedType={feedType} />
